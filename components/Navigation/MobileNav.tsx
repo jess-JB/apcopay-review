31b2c37454fd13d912f04e20d97d37f1318d0ff0
jess-JB <jessmark.baguio@webeelabs.com.mt>
2024-07-02 15:05:33 +0800

    test

--- components/Navigation/MobileNav.tsx
+++ components/Navigation/MobileNav.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import ImageWithFallback from "../ImageWithFallback/ImageWithFallback";
-import LanguageSelector from "../LanguageSelector/LanguageSelector";
-import Divider from "../Divider/Divider";
-import Typography from "../Typography";
 import Button from "../Button/Button";
-import CustomLink from "../CustomLink/CustomLink";
 
 const MobileNav = () => {
   return (
     <div>
       <div className='desktop:hidden max-w-[1440px] m-auto px-5 flex items-center justify-between gap-5 py-[1.875rem]  overflow-hidden'>
-        <ImageWithFallback
-          src={"/assets/nav-logo.svg"}
-          width={212.78}
-          height={35}
-        />
-
+        <div className='flex gap-8'>
+          <ImageWithFallback
+            src={"/assets/icon/mobile-menu-icon.svg"}
+            width={19}
+            height={19}
+          />
+          <ImageWithFallback
+            src={"/assets/icon/mobile-apcopay-logo.svg"}
+            width={40}
+            height={43}
+          />
+        </div>
         <Button variant={"gradient"}>Start Now</Button>
       </div>
     </div>
